30386b2d0d8cc6ddbbe27b31e9c58d9e0a3dd2e3
MOHAMED FAROUK <59924879+Medo3-coder@users.noreply.github.com>
2025-11-13 22:44:51 +0200

    User Data Modelling

---
+++ backend/src/models/User.js
@@ -0,0 +1,101 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "First name is required"],
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: [true, "Last name is required"],
+      trim: true,
+    },
+    image: {
+      type: String,
+      default:
+        "https://media.istockphoto.com/id/1451587807/vector/user-profile-icon-vector-avatar-or-person-icon-profile-picture-portrait-symbol-vector.jpg?s=2048x2048&w=is&k=20&c=-g-2McKwLpsyYHPDT3Wf1oo2ppTmNxq797heiFJmwSM=",
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    bio: {
+      type: String,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters long"],
+      select: false, // Exclude from query results by default ✅ Hide password by default
+    },
+    postCount: {
+      type: Number,
+      default: 0,
+    },
+    isBlocked: {
+      type: Boolean,
+      default: false,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    role: {
+      type: String,
+      enum: ["Admin", "Guest", "Blogger"],
+      default: "Guest",
+    },
+    isFollowing: {
+      type: Boolean,
+      default: false,
+    },
+    isUnFollowing: {
+      type: Boolean,
+      default: false,
+    },
+    isAccountVerified: {
+      type: Boolean,
+      default: false,
+    },
+    accountVerificationToken: String,
+    accountVerificationTokenExpires: Date,
+
+    viewedBy: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetTokenExpires: Date,
+
+    active: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
